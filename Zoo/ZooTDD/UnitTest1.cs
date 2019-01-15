fd33cb6dd4737e2c6f44abce761019cf006eb978
ntibbals <ntibbals@gmail.com>
2019-01-14 20:55:50 -0800

    lion tests passing

--- Zoo/ZooTDD/UnitTest1.cs
+++ Zoo/ZooTDD/UnitTest1.cs
@@ -18,9 +18,28 @@ namespace ZooTDD
         [Fact]
         public void TigerBirthing()
         {
-            /// Test that tiger eating exhibits proper behavior
+            /// Test that tiger birthing exhibits proper behavior
             BengalTiger tiger = new BengalTiger();
             Assert.True(tiger.Birthing());
+        }
+
+
+        [Fact]
+        public void LionRoaming()
+        {
+            /// Test that lion roamingexhibits proper behavior
+            Lion lion = new Lion();
+            string testRoam = "Just roaming around....";
+            Assert.Equal(testRoam, lion.Roam());
+        }
+
+        [Fact]
+        public void LionSleeping()
+        {
+            /// Test that lion sleeping exhibits proper behavior
+            Lion lion = new Lion();
+            string testSleep = "Me sleep.";
+            Assert.Equal(testSleep, lion.Sleeping());
         }
     }
 }
