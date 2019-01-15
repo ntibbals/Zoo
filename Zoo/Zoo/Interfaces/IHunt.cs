175db493dd25024533b4dcc1b354380c042c94d6
ntibbals <ntibbals@gmail.com>
2019-01-15 11:05:08 -0800

    setup interfaces

---
+++ Zoo/Zoo/Interfaces/IHunt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Interfaces
+{
+    interface IHunt
+    {
+        string Hunting();
+        bool FoundPrey();
+        bool MadeKill();
+    }
+}
